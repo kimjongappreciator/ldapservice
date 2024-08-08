fb9cdebbfb9232b30c1641e390a402c09bad8dd8
Sebastian <nonexistentexistentialist1@gmail.com>
2024-08-08 12:08:57 -0500

    Update Claves.java

--- src/main/java/com/unmsm/ldapservice/helper/Claves.java
+++ src/main/java/com/unmsm/ldapservice/helper/Claves.java
@@ -21,7 +21,7 @@ public class Claves {
         md.update(sPassword.getBytes("UTF-8"));
         byte[] raw = md.digest();
         Base64.Encoder base64 = Base64.getEncoder();
-        System.out.println(base64.encode(raw));
+        System.out.println(raw.toString());
         String result = "{MD5}" + base64.encode(raw);
         System.out.println(result);
         return result;

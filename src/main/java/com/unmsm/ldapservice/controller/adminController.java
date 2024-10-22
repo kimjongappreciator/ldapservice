f9b69270239c59ba71f38f1c7a7d301c5706d1cf
Sebastian Chacon <nonexistentexistentialist1@gmail.com>
2024-10-22 09:20:59 -0500

    Update adminController.java

--- src/main/java/com/unmsm/ldapservice/controller/adminController.java
+++ src/main/java/com/unmsm/ldapservice/controller/adminController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Retention;
 import java.util.List;
 @CrossOrigin
 @RestController
@@ -222,6 +223,123 @@ public class adminController {
 
     }
 
+    @PostMapping(path = "/crearGoogleDocente")
+    public ResponseEntity<String> crearGoogleDocente(@RequestBody Usuario usua) throws Exception{
+        String sEmail = usua.getCorreo_sm() + "@unmsm.edu.pe";
+        String apellidos = usua.getApellido_paterno() + " " + usua.getApellido_materno();
+        String uString = "";
+        try{
+            uString = this.google.obtenerUsuario(sEmail);
+            if(uString == null){
+                this.google.agregarUsuario(apellidos, usua.getNombres(), sEmail, usua.getPass(), "Docentes");
+                return ResponseEntity.status(HttpStatus.OK).body("Usuario creado exitosamente");
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario ya existe");
+            }
+        } catch (Throwable e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el usuario");
+        }
+    }
 
+    @PostMapping(path = "/crearGoogleAlumno")
+    public ResponseEntity<String> crearGoogleAlumno(@RequestBody Usuario usua) throws Exception{
+        String sEmail = usua.getCorreo_sm() + "@unmsm.edu.pe";
+        String apellidos = usua.getApellido_paterno() + " " + usua.getApellido_materno();
+        String uString = "";
+        String situacion = usua.getSituacion();
+        String sGrupo = "";
+
+        if(situacion == "2"){
+            sGrupo = "EGRESADOS";
+        }
+        else if(usua.getDesc_tipo_usua().equals("Pregrado")){
+            sGrupo = "PREGRADOM";
+        }
+        else if(usua.getDesc_tipo_usua().equals("Posgrado")){
+            sGrupo = "POSGRADOM";
+        }
+        else{
+            sGrupo = "Users";
+        }
+        try{
+            uString = this.google.obtenerUsuario(sEmail);
+            if(uString == null){
+                this.google.agregarUsuario(apellidos, usua.getNombres(), sEmail, usua.getPass(), sGrupo);
+                return ResponseEntity.status(HttpStatus.OK).body("Usuario creado exitosamente");
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario ya existe");
+            }
+        } catch (Throwable e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el usuario");
+        }
+    }
+
+    @PostMapping(path = "/crearldapdocente")
+    public ResponseEntity<String> crearLdapDocente(@RequestBody Usuario usua) {
+        String sEmail = usua.getCorreo_sm() + "@unmsm.edu.pe";
+        Utils util = new Utils();
+        String Status;
+        String apellidos = usua.getApellido_paterno() + " " + usua.getApellido_materno();
+        String facu = util.buscarfacultad(usua.getDesc_facu());
+        String tipo = "Docente";
+
+        if(facu == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la facultad");
+        }
+
+        try {
+            int usuario = this.ldap.Buscar(usua.getCorreo_sm());
+            if (usuario < 1) {
+                this.ldap.agregarUsuarioDocente(1, "0", usua.getCorreo_sm(), usua.getUidNumber(), "0", usua.getCod_usua(), apellidos,
+                        usua.getNombres(), sEmail, usua.getPass(), usua.getDesc_facu(),
+                        tipo + " " + facu, tipo.toUpperCase());
+                //this.ldap.cambioClave(usua.getCorreo_sm(), usua.getPass());
+
+                Status = "El usuario " + sEmail +" fue creado";
+                return ResponseEntity.ok(Status);
+            } else {
+                Status = "El usuario " + sEmail +" ya existe";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Status);
+            }
+        } catch (Throwable ex) {
+            Status = "Error: " + ex.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Status);
+        }
+    }
+
+    @PostMapping(path = "/crearldapestudiante")
+    public ResponseEntity<String> crearLdapEstudiante(@RequestBody Usuario usua) {
+        Utils util = new Utils();
+        String Status;
+        String sEmail = usua.getCorreo_sm() + "@unmsm.edu.pe";
+        String apellidos = usua.getApellido_paterno() + " " + usua.getApellido_materno();
+
+        String facu = util.buscarfacultad(usua.getDesc_facu());
+
+        if(facu == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error en la facultad");
+        }
+        String Stipo = usua.getDesc_tipo_usua() + " " + facu;
+
+
+        try {
+            String usuario = this.google.obtenerUsuario(usua.getCorreo_sm() + "@unmsm.edu.pe");
+            if (usuario == null) {
+                this.ldap.agregarUsuario(usua.getCorreo_sm(), usua.getUidNumber(), "0", usua.getCod_usua(), apellidos,
+                        usua.getNombres(), sEmail, usua.getNum_doc(), usua.getDesc_facu(), Stipo, usua.getPass());
+
+                Status = "El usuario " + sEmail +" fue creado";
+                return ResponseEntity.ok(Status);
+            } else {
+                Status = "El usuario " + sEmail +" ya existe";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Status);
+            }
+        } catch (Throwable e) {
+            Status = "Error: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Status);
+        }
+    }
 
 }
